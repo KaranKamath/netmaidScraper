299e17aba42bbac856503286d5da33ff9dcc0a4a
Karan Kamath <karankamath26@gmail.com>
2014-08-02 17:44:12 +0800

    Maid details print ex

--- main_script.py
+++ main_script.py
@@ -1,2 +1,19 @@
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
+
+EX_URL = "http://netmaid.com.sg/maids/272739"
+
+def makeSoup(URL):
+    html = urlopen(URL).read()
+    soup = BeautifulSoup(html, "lxml")
+    return soup;
+
+def main():
+    soup = makeSoup(EX_URL)
+
+    maidDetails = soup.find(id="maid_detail")
+
+    print maidDetails
+
+if __name__ == "__main__":
+    main()
