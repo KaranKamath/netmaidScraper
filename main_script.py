70553bee0293484155b5b1bb282f9b35c0fc2a81
Karan Kamath <karankamath26@gmail.com>
2014-08-03 17:48:26 +0800

    Parsing out table for Prefs

--- main_script.py
+++ main_script.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 from urllib2 import urlopen
 from pprint import pprint
 
-EX_URL = "http://netmaid.com.sg/maids/272739"
+EX_URL = "http://netmaid.com.sg/maids/273159"    #272739
 
 def makeSoup(URL):
     html = urlopen(URL).read()
@@ -15,7 +15,12 @@ def drinkSoup(soup):
     maidDict = {}
 
     for childDiv in maidDetails.find_all("div", "title"):
-        maidDict[childDiv.text] = childDiv.find_next_sibling("div").text
+        fieldName = childDiv.text.replace("&"," &")
+
+        if "& Experience" in fieldName:
+            print childDiv.find_next_sibling(True)
+        else:
+            maidDict[fieldName] = childDiv.find_next_sibling("div").text
 
     pprint(maidDict);
 
