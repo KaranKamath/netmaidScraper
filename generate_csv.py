53249cf3387cb3101966c03b4ada96c86d3f4ca3
Karan Kamath <karankamath26@gmail.com>
2014-09-08 01:06:42 +0800

    Print table headers

--- generate_csv.py
+++ generate_csv.py
@@ -14,7 +14,45 @@ strdate = datetime.now().strftime("%d %B, %X")
 
 with open('output-'+ strdate + '.csv', 'wb') as f:
     csv_out = csv.writer(f, dialect='excel')
-
+    header_list = [
+            "urlID",
+            "Reference Code",
+            "Type",
+            "Base Salary",
+            "Rest Day Preference",
+            "Maid Agency",
+            "Nationality",
+            "Date of Birth",
+            "Place of Birth",
+            "Siblings",
+            "Height",
+            "Weight",
+            "Religion",
+            "Marital Status",
+            "Children",
+            "Education",
+            "Language Skill",
+            "Cares For Children",
+            "Cares For Elderly",
+            "Cares For Disabled",
+            "Prefers Housework",
+            "Prefers Cooking",
+            "Can Handle Pork",
+            "Can Eat Pork",
+            "Can Handle Beef",
+            "Cares for Dogs or Cats",
+            "Does Gardening",
+            "Does Sewing",
+            "Washes Car",
+            "Works Off Days For Compensation",
+            "Working Experience",
+            "Maid Introduction",
+            "Image Path",
+            "Init Date",
+            "As Of Date",
+            "Expiry Date"
+            ]
+    csv_out.writerow(header_list)
     while row:
         formatted_row = []
         for r in row:
